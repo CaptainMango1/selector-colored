47b7a2d4b278089d0dc4351148e9f9d3a214ba07
mangoeis <60978002+CaptainMango1@users.noreply.github.com>
2022-09-13 03:00:27 +0200

    finished commit

--- selector.js
+++ selector.js
@@ -1,7 +1,7 @@
-const colors = ["#96adfb", "#fbe496", "#fa96c1", "#96facf", "#b7fa96", "#fac496", "#96dcfa"];
+let colors = ['#96adfb', '#fbe496', '#fa96c1', '#96facf', '#b7fa96', '#fac496', '#96dcfa'];
 
-window.addEventListener("mousedown", () => {
+window.addEventListener('mousedown', () => {
     var color = colors.shift();
-    document.documentElement.style.setProperty("--highlight", color);
+    document.documentElement.style.setProperty('--highlight', color);
     colors.push(color);
 });
